cd4d5cd1a0896713f40c8ac964c449994656b5a0
Nichlas Torgersen <fristurt@hotmail.com>
2015-05-28 16:36:21 +0200

    setup project

---
+++ app/bootstrap.php
@@ -0,0 +1,2 @@
+<?php
+    /* brukes som en hub for å require alle filer vi trenger slik at vi ikke behøver å skrive require overalt mer enn èn gang */
---
+++ app/config/Config.php
@@ -0,0 +1,8 @@
+class Config {
+    
+    public static $DATABASE_HOST = 'localhost';
+    public static $DATABASE_USERNAME = 'root';
+    public static $DATABASE_PASSWORD = '';
+    public static $DATABASE_DB_NAME = 'web-is-gr11w';
+
+}
